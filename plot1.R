103fb1b0943d5d1eca66e6d5e78a3f891fa3a92b
chellakumarmani <40386328+chellakumarmani@users.noreply.github.com>
2018-08-28 20:25:10 +0530

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,18 @@
+plot1<-function() {
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip","household_power_consumption.zip")
+unzip(zipfile="household_power_consumption.zip")
+power<-read.table("household_power_consumption.txt",stringsAsFactors = FALSE,header=TRUE,sep=";")
+power$Date <- as.Date(power$Date, format="%d/%m/%Y")
+power$Time <- format(power$Time, format="%H:%M:%S")
+power$Global_active_power <- as.numeric(power$Global_active_power)
+power$Global_reactive_power <- as.numeric(power$Global_reactive_power)
+power$Voltage <- as.numeric(power$Voltage)
+power$Global_intensity <- as.numeric(power$Global_intensity)
+power$Sub_metering_1 <- as.numeric(power$Sub_metering_1)
+power$Sub_metering_2 <- as.numeric(power$Sub_metering_2)
+power$Sub_metering_3 <- as.numeric(power$Sub_metering_3)
+subpower <- subset(power, Date == "2007-02-01" | Date =="2007-02-02")
+png("plot1.png", width=480, height=480)
+hist(subpower$Global_active_power, col="red", border="black", main ="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency")
+dev.off()
+}
